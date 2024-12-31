50733136a1d9b231b8a85ec73b840dd440d6848c
Hskcodes <hskdashing@aol.com>
2024-12-31 17:25:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 from mega import Mega
-from telegram import Update
+from telegram import Update  # Yeh line add ki gayi hai
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 # Bot Token (Telegram)
@@ -47,8 +47,8 @@ def rename(update: Update, context: CallbackContext) -> None:
         # Log in to Mega
         m = mega_login()
 
-        # Get folder ID using Mega API's `find` method (search folder by URL)
-        folder = m.find(folder_url)[0]  # Assumes the first result is the folder you're looking for
+        # Get folder ID and rename folder
+        folder = m.get_folder(folder_url)  # Assuming the function is correct for getting folder
         
         # Renaming the main folder
         m.rename(folder, new_folder_name)
